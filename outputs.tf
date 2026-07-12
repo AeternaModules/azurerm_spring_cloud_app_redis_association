26a97b62d5d731854ff29351ecf749b166958b4a
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:31:49 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "spring_cloud_app_redis_associations_id" {
+  description = "Map of id values across all spring_cloud_app_redis_associations, keyed the same as var.spring_cloud_app_redis_associations"
+  value       = { for k, v in azurerm_spring_cloud_app_redis_association.spring_cloud_app_redis_associations : k => v.id }
+}
 output "spring_cloud_app_redis_associations_name" {
   description = "Map of name values across all spring_cloud_app_redis_associations, keyed the same as var.spring_cloud_app_redis_associations"
   value       = { for k, v in azurerm_spring_cloud_app_redis_association.spring_cloud_app_redis_associations : k => v.name }
